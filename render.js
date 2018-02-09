8fcae9f2c991c2dfa552820ed3a7a016187b0ab3
Kevin Durbin <kevindurb@gmail.com>
2018-02-09 08:49:44 -0700

    Extract jss to inject into index.html

--- render.js
+++ render.js
@@ -1,23 +1,35 @@
+import React from 'react';
 import { render } from 'react-dom';
 import { renderToString } from 'react-dom/server';
+import {JssProvider, SheetsRegistry} from 'react-jss';
 import indexTemplate from './templates/index.ejs';
 
 export default function (component) {
   if (typeof document !== 'undefined') {
     render(
-      component,
+      <JssProvider>
+        {component}
+      </JssProvider>,
       document.getElementById('root')
     );
   }
 
   return (locals) => {
-    const content = renderToString(component);
+    const sheets = new SheetsRegistry();
+
+    const content = renderToString(
+      <JssProvider registry={sheets}>
+        {component}
+      </JssProvider>
+    );
+
     return indexTemplate({
       content,
       scripts: [
         locals.assets.manifest,
         locals.assets.main,
-      ]
+      ],
+      styles: sheets.toString(),
     });
   };
 }
